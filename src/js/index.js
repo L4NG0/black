af5d4ba68c0fa8fd6cfc62a493b17f04da020cc6
Oskar Lango <langooskar@gmail.com>
2024-03-03 22:20:22 +0100

    Title: Enhance Navigation Visibility, Support Localization, and Refine Styles
    Body:
    - Implement `toggleVisibility` to enhance the responsiveness and visibility of navigation and footer elements across different screen sizes, utilizing animation for a smoother user experience.
    - Introduce `loadLanguage` function to support dynamic content localization, allowing users to switch between English and Polish languages seamlessly. Fetches translations from `pl.json` and applies them to elements marked with `data-translate`.
    - Adjust CSS for `.navigation__icon`, `.navigation__box`, and `.footer` to improve layout and visibility on larger screens, ensuring consistency across different device sizes.
    - Modify `.section-start__logo` source sets for higher resolution displays, ensuring crisp visuals for high DPI devices.
    - Optimize `underlineUsingIntersectionObserver` for dynamic underline effects in the navigation menu, improving user interaction by highlighting the current section.
    - Enable language switch buttons (`#buttonEn`, `#buttonPl`) to dynamically load and apply language settings, enhancing the site's accessibility and user engagement.
    - Streamline DOMContentLoaded event handling to initialize localization and interactive features, ensuring a polished user experience from the first load.

--- src/js/index.js
+++ src/js/index.js
@@ -1,57 +1,84 @@
-
+function loadLanguage(lang) {
+    document.querySelectorAll('[data-translate]').forEach(el => {
+        if (!el.getAttribute('data-original')) {
+            el.setAttribute('data-original', el.textContent);
+        }
+    });
+    function restoreOriginalLanguage() {
+        document.querySelectorAll('[data-translate]').forEach(el => {
+            const originalContent = el.getAttribute('data-original');
+            if (originalContent) {
+                el.textContent = originalContent;
+            }
+        });
+    }
+    console.log(document.documentElement.lang);
+    if (lang === document.documentElement.lang) {
+        restoreOriginalLanguage();
+        return;
+    }
+    console.log(lang);
+    fetch(`translations/${lang}.json`)
+        .then(response => response.json())
+        .then(translations => {
+            document.querySelectorAll('[data-translate]').forEach(el => {
+                const key = el.getAttribute('data-translate');
+                if (translations[key]) {
+                    el.textContent = translations[key];
+                }
+            });
+        })
+        .catch(error => console.error('Error loading the translation', error));
+}
 function toggleVisibility() {
     const media = window.matchMedia("(max-width: 1025px)")
     if (media.matches) {
         const navigationBox = document.querySelector('.navigation__box');
-        const menu = document.querySelector('.navigation__menu');
+        const footer = document.querySelector('.footer');
         const iconXmark = document.querySelector('.navigation__icon-xmark');
         const navIcon = document.querySelectorAll('.navigation__icon');
         const banner = document.querySelector('.header__banner');
+        const elementsToAnimate = [navigationBox, footer];
+
+        const animationKeyframesStart = [
+            { transform: 'translateY(-100%) scale(0.98)', opacity: 0, offset: 0 },
+            { transform: 'translateY(-2%) scale(0.98)', offset: 0.5 },
+            { transform: 'translateY(0) scale(1)', opacity: 1, offset: 1 },
+        ];
+        const animationKeyframesEnd = [
+            { transform: 'translateY(0) scale(1)', opacity: 1 },
+            { transform: 'translateY(-2%) scale(0.98)', offset: 0.5 },
+            { transform: 'translateY(-100%) scale(0.98)', opacity: 0 },
+        ];
+        const animationOptions = {
+            duration: 800,
+            fill: 'forwards'
+        };
+
+        function animateElements(elements, keyframes, options, onFinish) {
+            const animations = elements.map(element => element.animate(keyframes, options).finished);
+
+            Promise.all(animations).then(onFinish);
+        }
 
         navIcon.forEach(element => element.classList.toggle('visible'));
 
         if (iconXmark.classList.contains('visible')) {
-            menu.style.display = "block";
             navigationBox.style.display = "block";
-
+            footer.style.display = "block";
             banner.classList.add('header__banner-border');
-
-            const animationKeyframes = [
-                { transform: 'translateY(-100%) scale(0.98)', opacity: 0, offset: 0 },
-                { transform: 'translateY(-2%) scale(0.98)', offset: 0.5 },
-                { transform: 'translateY(0) scale(1)', opacity: 1, offset: 1 },
-            ];
-
-            const animationOptions = {
-                duration: 800,
-                fill: 'forwards'
-            };
-
-            const animation = navigationBox.animate(animationKeyframes, animationOptions);
+            animateElements(elementsToAnimate, animationKeyframesStart, animationOptions, () => { });
         }
         else {
-            const animationKeyframes = [
-                { transform: 'translateY(0) scale(1)', opacity: 1 },
-                { transform: 'translateY(-2%) scale(0.98)', offset: 0.5 },
-                { transform: 'translateY(-100%) scale(0.98)', opacity: 0 },
-            ];
-
-            const animationOptions = {
-                duration: 800,
-                fill: 'forwards'
-            };
-            const animation = navigationBox.animate(animationKeyframes, animationOptions);
-            animation.onfinish = () => {
-                menu.style.display = "none";
+            animateElements(elementsToAnimate, animationKeyframesEnd, animationOptions, () => {
                 navigationBox.style.display = "none";
+                footer.style.display = "none";
                 banner.classList.remove('header__banner-border');
-            }
-
-
-
-        };
-    }
+            });
+        }
+    };
 }
+
 
 function scroll(e) {
     e.preventDefault();
@@ -141,6 +168,14 @@ function underlineUsingIntersectionObserver() {
 
 document.addEventListener('DOMContentLoaded', () => {
     underlineUsingIntersectionObserver();
+    document.getElementById('buttonEn').addEventListener('click', function () {
+        loadLanguage('en');
+    });
+
+    document.getElementById('buttonPl').addEventListener('click', function () {
+        loadLanguage('pl');
+    });
+
     document.querySelectorAll('.navigation__item a').forEach(element => {
         element.addEventListener('click', scroll);
         element.addEventListener('click', toggleVisibility);
